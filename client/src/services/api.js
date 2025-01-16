e8d1774ab46b3acc7a84bcf96bfc55dc1681f6f2
Iskandar <75279834+aryanshoh@users.noreply.github.com>
2025-01-17 00:16:38 +0500

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/api';
+// const API_URL = 'http://localhost:3001/api';
+const API_URL = 'https://securenotes-backend-2cit.onrender.com/api';
 
 const api = axios.create({
     baseURL: API_URL
